13e4b5f02924617c5e1f07c96e7e47f7e464e063
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2016-09-28 19:08:18 -0700

    refactor registration

--- registration.py
+++ registration.py
@@ -1,14 +1,18 @@
 import requests
 
-def main():
-	registration()
-
-def registration():
+def registration(token, github):
 	authentication = {
-		'token': '055755d0bef8118fe9960bcf4f12b2be',
-		'github': 'https://github.com/alejandrogarciasalas/code2040'
+		'token': token,
+		'github': github
 	}
 	r = requests.post('http://challenge.code2040.org/api/register', json=authentication)
+	return r.text
+
+def main():
+	token = '055755d0bef8118fe9960bcf4f12b2be'
+	github = 'https://github.com/alejandrogarciasalas/code2040'
+	validation = registration(token, github)
+	print(validation)
 
 if __name__ == "__main__":
 	main()
