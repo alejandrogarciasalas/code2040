c4eb8dfe0ed80f3fd7832b3dad7f0b7c3e27f26b
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2016-09-28 18:28:44 -0700

    rename vars

--- registration.py
+++ registration.py
@@ -4,11 +4,11 @@ def main():
 	registration()
 
 def registration():
-	data = {
+	authentication = {
 		'token': '055755d0bef8118fe9960bcf4f12b2be',
 		'github': 'https://github.com/alejandrogarciasalas/code2040'
 	}
-	r = requests.post('http://challenge.code2040.org/api/register', json=data)
+	r = requests.post('http://challenge.code2040.org/api/register', json=authentication)
 
 if __name__ == "__main__":
 	main()
